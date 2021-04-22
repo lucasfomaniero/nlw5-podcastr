ae2c9aac60987c1e56d9813cc634f3a1f15ab59b
Lucas Maniero <lucasfomaniero@gmail.com>
2021-04-22 17:38:04 -0300

    Minor adjusts in Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
+import { GetStaticProps } from "next";
+import { useEffect } from "react";
 import { Header } from "../components/Header";
 
-export default function Home() {
+type Episode = {
+    id: string;
+    title: string;
+    members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[] // ou Array<Episode>
+}
+
+export default function Home(props: HomeProps) {
   return(
-    <p>teste</p>
+    <p>{JSON.stringify(props.episodes)}</p>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+  
+  return {
+    props: {
+    episodes: data,
+  }
+};
 }
